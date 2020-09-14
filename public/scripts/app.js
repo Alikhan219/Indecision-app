c8fa4a99224be959f59a61edb0b74fb2162fb30f
unknown <merabhai786786@gmail.com>
2020-09-14 02:54:34 -0700

    creating all different things and functions of application

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,36 @@
 'use strict';
 
-var template = React.createElement(
-  'p',
-  null,
-  'Is this working'
-);
+var visibility = false;
+var visibilityTog = function visibilityTog() {
+    visibility = !visibility;
+    render();
+};
+var render = function render() {
 
-var root = document.querySelector('#mainDiv');
-ReactDOM.render(template, root);
+    var visible = React.createElement(
+        'div',
+        null,
+        React.createElement(
+            'h1',
+            null,
+            'Visibility Toggle'
+        ),
+        React.createElement(
+            'button',
+            { onClick: visibilityTog },
+            visibility ? 'Hide Details' : 'Show Details'
+        ),
+        visibility && React.createElement(
+            'div',
+            null,
+            React.createElement(
+                'p',
+                null,
+                'Hey There are some details that you have to know.'
+            )
+        )
+    );
+    ReactDOM.render(visible, document.getElementById('mainDiv'));
+};
+
+render();
